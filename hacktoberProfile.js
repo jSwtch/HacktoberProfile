54436f1176760f7d1f8aecfe34434d4c56dea165
jSwtch <sanderson.jma@gmail.com>
2019-10-30 00:12:20 -0600

    Update profile
    added profile details: name, place, user

--- hacktoberProfile.js
+++ hacktoberProfile.js
@@ -678,5 +678,10 @@ var profiles = [
     name : "pittyi2nd",
     place : "Hyogo, Japan",
     github : "pittyi2nd"
+  },
+    {
+    name : "Jonathan Sanderson",
+    place : "Edmonton, Canada",
+    github : "jSwtch"
   }
 ];
